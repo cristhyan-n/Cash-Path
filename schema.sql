c2187bcc009256377345c6196c8ce0c43f2e0c5c
cristhyan-n <cristhyan.vo@gmail.com>
2026-09-17 19:58:24 -0300

    tabela usuario criado

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE usuario (
+    id INTEGER PRIMARY KEY NOT NULL,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    senha TEXT NOT NULL,
+    role TEXT NOT NULL DEFAULT 'usuario',
+    CHECK (role in ('usuario', 'admin'))
+);
+
