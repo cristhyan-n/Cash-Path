773965579742886099c6f0f7d84f0f70708aaea6
cristhyan-n <cristhyan.vo@gmail.com>
2026-09-17 20:00:13 -0300

    tabela meta criado

--- schema.sql
+++ schema.sql
@@ -20,3 +20,12 @@ CREATE TABLE transacao (
     usuario_id INTEGER NOT NULL REFERENCES usuario(id),
     categoria_id INTEGER NOT NULL REFERENCES categoria(id)
 );
+
+CREATE TABLE meta (
+    id INTEGER PRIMARY KEY NOT NULL,
+    valor_alvo INTEGER NOT NULL,
+    data_inicio TEXT NOT NULL,
+    status TEXT NOT NULL DEFAULT 'ativo' CHECK ( status in ('ativo', 'concluido')),
+    tipo TEXT NOT NULL CHECK ( tipo in ('economizar', 'investir')),
+    usuario_id INTEGER NOT NULL REFERENCES usuario(id)
+);
