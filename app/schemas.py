d31bf15063cf494f118e8516d9faba08d819044e
cristhyan-n <cristhyan.vo@gmail.com>
2026-09-18 19:09:13 -0300

    cria schemas de entrada e saida para cadastro de usuario

---
+++ app/schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+class CadastroUsuario(BaseModel):
+    nome: str
+    email: str
+    senha: str
+
+
+class UsuarioResponse(BaseModel):
+    id: int
+    nome: str
+    email: str
+
+    class Config:
+        from_attributes = True
