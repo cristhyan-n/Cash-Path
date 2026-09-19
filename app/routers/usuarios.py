89cd30c4fee4426843659bca5141c8a2ee0bc38c
cristhyan-n <cristhyan.vo@gmail.com>
2026-09-18 21:22:29 -0300

    att

--- app/routers/usuarios.py
+++ app/routers/usuarios.py
@@ -12,6 +12,7 @@ router = APIRouter()
 @router.post("/usuarios", response_model=UsuarioResponse)
 def criar_usuario(dados_usuario: CadastroUsuario, conexao=Depends(get_db_connection)):
     cursor = conexao.cursor()
+
     try:
         cursor.execute(
             "INSERT INTO usuario (nome, email, senha) VALUES (?, ?, ?)",
@@ -25,11 +26,5 @@ def criar_usuario(dados_usuario: CadastroUsuario, conexao=Depends(get_db_connect
         novo_id = cursor.lastrowid
     except sqlite3.IntegrityError:
         raise HTTPException(status_code=400, detail="Email já cadastrado.")
-    cursor.execute(
-        "INSERT INTO usuario (nome, email, senha) VALUES (?, ?, ?)",
-        (dados_usuario.nome, dados_usuario.email, hash_password(dados_usuario.senha)),
-    )
-    conexao.commit()
-    novo_id = cursor.lastrowid
 
     return {"id": novo_id, "nome": dados_usuario.nome, "email": dados_usuario.email}
